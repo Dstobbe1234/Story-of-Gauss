8f9e85b674ef765e8388bb97e4b760fd2f1cefa6
David Stobbe <d.stobbe1@share.epsb.ca>
2022-03-01 15:50:49 -0700

    Good enough

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Script.js
+
+// Story of Gauss - David Stobbe
+
+// HTML Elements
+let calcBtn = document.getElementById("calc-btn");
+let sumOut = document.getElementById("sum-out");
+let nInput = document.getElementById("n-in");
+let nOut = document.getElementById("n-out");
+
+// Add event listener
+calcBtn.addEventListener("click", calcSum)
+
+// Event Function 
+function calcSum() {
+    let total = nInput.value/2 * (nInput.value + 1);
+    // Output the total
+    sumOut.innerHTML = total;
+    nOut.innerHTML = nInput.value;
+}
